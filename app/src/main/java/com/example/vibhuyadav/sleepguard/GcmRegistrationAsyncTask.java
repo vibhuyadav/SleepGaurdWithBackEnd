dd0b97aeb942879d81de762f2b91e8a2e97ef285
Vibhu Yadav <vib.yad@gmail.com>
2015-03-04 02:21:34 -0500

    Merge Changes

--- app/src/main/java/com/example/vibhuyadav/sleepguard/GcmRegistrationAsyncTask.java
+++ app/src/main/java/com/example/vibhuyadav/sleepguard/GcmRegistrationAsyncTask.java
@@ -22,7 +22,7 @@ public class GcmRegistrationAsyncTask extends AsyncTask<Void, Void, String> {
     private Context context;
 
     // TODO: change to your own sender ID to Google Developers Console project number, as per instructions above
-    private static final String SENDER_ID = "530139348531";//Vihub:"530139348531" Wei:"454255453333"
+    private static final String SENDER_ID = "530139348531";//Vibhu:"530139348531" Wei:"454255453333"
 
     public GcmRegistrationAsyncTask(Context context) {
         this.context = context;
@@ -35,7 +35,7 @@ public class GcmRegistrationAsyncTask extends AsyncTask<Void, Void, String> {
                     //.setRootUrl("https://stable-synapse-857.appspot.com/_ah/api/");
                     .setRootUrl("https://praxis-practice-856.appspot.com/_ah/api/");
             // end of optional local run code
-            //Vihub:"https://praxis-practice-856.appspot.com/_ah/api/"
+            //Vibhu:"https://praxis-practice-856.appspot.com/_ah/api/"
             //Wei  :"https://stable-synapse-857.appspot.com/_ah/api/"
             regService = builder.build();
         }
