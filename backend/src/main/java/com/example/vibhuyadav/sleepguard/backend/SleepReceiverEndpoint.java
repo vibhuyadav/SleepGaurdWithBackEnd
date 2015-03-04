d79015807ca4d9567aed4230cf16286776bb459a
hw87244778 <hw87244557@163.com>
2015-03-04 01:22:29 -0500

    Wei Changes

---
+++ backend/src/main/java/com/example/vibhuyadav/sleepguard/backend/SleepReceiverEndpoint.java
@@ -0,0 +1,36 @@
+package com.example.vibhuyadav.sleepguard.backend;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.inject.Named;
+import javax.xml.stream.events.EndDocument;
+
+import sun.rmi.transport.Endpoint;
+
+/**
+ * An endpoint class we are exposing
+ */
+@Api(
+        name = "sleepReceiverApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "backend.sleepguard.vibhuyadav.example.com",
+                ownerName = "backend.sleepguard.vibhuyadav.example.com",
+                packagePath = ""
+        )
+)
+public class SleepReceiverEndpoint {
+
+    private static final Logger logger = Logger.getLogger(SleepReceiverEndpoint.class.getName());
+
+    @ApiMethod(name = "sendTimeStamp")
+    public void sendTimeStamp(@Named("regId") String regId,@Named("timeStamp") String timeStamp) throws IOException {
+        MessagingEndpoint messagingEndpoint=new MessagingEndpoint();
+        messagingEndpoint.sendMessage(timeStamp);
+    }
+}
