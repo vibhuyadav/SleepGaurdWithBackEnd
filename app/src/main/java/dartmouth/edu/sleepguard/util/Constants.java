01f71ee7d059b541b2e0c253143d2795dfaa231e
Vibhu Yadav <vib.yad@gmail.com>
2015-03-05 10:44:55 -0500

    Update interval changed

--- app/src/main/java/dartmouth/edu/sleepguard/util/Constants.java
+++ app/src/main/java/dartmouth/edu/sleepguard/util/Constants.java
@@ -25,7 +25,7 @@ public class Constants {
     /**
      * The desired interval for location updates. Inexact. Updates may be more or less frequent.
      */
-    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 5000;
+    public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
 
     /**
      * The fastest rate for active location updates. Exact. Updates will never be more frequent
