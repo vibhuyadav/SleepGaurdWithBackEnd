d52209df54e1f342b0433fa97816c24f4ca1d3a0
Vibhu Yadav <vib.yad@gmail.com>
2015-03-08 05:09:07 -0400

    Merge.

--- app/src/main/java/com/example/vibhuyadav/sleepguard/AudioWindow.java
+++ app/src/main/java/com/example/vibhuyadav/sleepguard/AudioWindow.java
@@ -13,41 +13,41 @@ public class AudioWindow {
     SampleNode bottom;
     SampleNode iterator;
 
-    public AudioWindow (short threshold,int seconds,int sampleRate){
-        top=null;
-        bottom=null;
-        num_of_nodes=0;
-        num_over_threshold=0;
-        cumulate=0;
-        this.threshold=threshold;
-        width=seconds*sampleRate;
+    public AudioWindow(short threshold, int seconds, int sampleRate) {
+        top = null;
+        bottom = null;
+        num_of_nodes = 0;
+        num_over_threshold = 0;
+        cumulate = 0;
+        this.threshold = threshold;
+        width = seconds * sampleRate;
     }
 
-    public void push(short a,long t){
-        SampleNode tempNode=new SampleNode(a,t);
-        if(Math.abs(a)>threshold)
+    public void push(short a, long t) {
+        SampleNode tempNode = new SampleNode(a, t);
+        if (Math.abs(a) > threshold)
             num_over_threshold++;
-        cumulate=cumulate+a*a;
-        if(num_of_nodes==0){
-            top=bottom=tempNode;
-        }else{
-            bottom.next=tempNode;
-            bottom=tempNode;
+        cumulate = cumulate + a * a;
+        if (num_of_nodes == 0) {
+            top = bottom = tempNode;
+        } else {
+            bottom.next = tempNode;
+            bottom = tempNode;
         }
         num_of_nodes++;
-        if(num_of_nodes>width)
+        if (num_of_nodes > width)
             pop();
     }
 
-    public boolean isEmpty(){
-        if(num_of_nodes==0)
+    public boolean isEmpty() {
+        if (num_of_nodes == 0)
             return true;
         else
             return false;
     }
 
-    public void pop(){
-        if(!isEmpty()) {
+    public void pop() {
+        if (!isEmpty()) {
             short temp = top.value;
             top = top.next;
             num_of_nodes--;
@@ -57,43 +57,51 @@ public class AudioWindow {
         }
     }
 
-    public boolean isFull(){
-        if(num_of_nodes==width){
+    public boolean isFull() {
+        if (num_of_nodes == width) {
             return true;
-        }
+        } else
+            return false;
+    }
+
+    public void setIterator() {
+        iterator = new SampleNode((short) 0, (long) 0);
+        iterator.next = top;
+    }
+
+    public boolean hasNext() {
+        if (iterator.next != null)
+            return true;
         else
             return false;
     }
 
-    public void setIterator(){
-        iterator=new SampleNode((short)0,(long)0);
-        iterator.next=top;
-    }
-    public boolean hasNext(){
-        if(iterator.next!=null)
-            return true;
-        else
-            return false;
-    }
-    public String[] getNext(){
-        iterator=iterator.next;
-        String[] strs={Short.toString(iterator.value),Long.toString(iterator.timeMills)};
+    public String[] getNext() {
+        iterator = iterator.next;
+        String[] strs = {Short.toString(iterator.value), Long.toString(iterator.timeMills)};
         return strs;
     }
 
-    public long getTimeStamp(){
+    public long getTimeStamp() {
         return top.timeMills;
     }
-    public int getNumOverThreshold(){ return num_over_threshold;}
-    public int getAverageAmplitude(){ return (int)(cumulate/((long)num_of_nodes));}
 
-    private class SampleNode{
+    public int getNumOverThreshold() {
+        return num_over_threshold;
+    }
+
+    public int getAverageAmplitude() {
+        return (int) (cumulate / ((long) num_of_nodes));
+    }
+
+    private class SampleNode {
         short value;
         long timeMills;
         SampleNode next;
-        public SampleNode(short v,long t){
-            timeMills=t;
-            value=v;
+
+        public SampleNode(short v, long t) {
+            timeMills = t;
+            value = v;
         }
     }
 }
